4235538852c231538bafadc820ebfcf5a187d911
David Volm <david@volminator.com>
2012-11-07 13:35:53 +0000

    Wierd bug with my editor messed up commit: d585a6cd5d57f1c3d44af3ecd508e5ecdcbf06b8

--- index.php
+++ index.php
@@ -1,1 +1,141 @@
-<!DOCTYPE html><html><head><title>daXXog.com — homepage of David Volm</title><link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono' rel='stylesheet' type='text/css'><link type="text/css" rel="stylesheet" href="./daxxog.min.css"><!--[if !IE]>--><link type="text/css" rel="stylesheet" href="./notie.min.css"><link type="text/css" rel="stylesheet" media="only screen and (max-device-width: 480px)" href="./iphone.min.css"><!--<![endif]--><link rel="icon" type="image/vnd.microsoft.icon" href="./favicon.ico"><meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0"><script type="text/javascript" src="./jquery-1.8.2.min.js"></script><script type="text/javascript">$(function(){$("div").animate({opacity:.95},1e3),$("body").scrollTop(1)})</script></head><body><div id="name">David Volm</div><div class="hashtag"> <a class="hashtag" href="https://twitter.com/daxxog" target="_blank">@daxxog</a></div><div id="bio">I am a problem solver; <a class="bio" href="https://github.com/daxxog" target="_blank">in code</a> and everyday life. Self motivation and teamwork are critical skills. <a  class="bio" href="http://lcms.org/" target="_blank">Jesus Christ died to save me.</a></div><div id="programminglanguages" class="listcon"> <span class="blockt">Programming languages:</span> <br><div class="fleft">{</div><div class="inblock"> <br> <a href="https://developer.mozilla.org/en-US/docs/JavaScript" class="ref" target="_blank">javascript</a>, <br> <a href="http://www.oracle.com/technetwork/java/index.html" class="ref" target="_blank">java</a>, <br> <a href="http://php.net/" class="ref" target="_blank">php</a>, <br> <a href="http://gcc.gnu.org/" class="ref" target="_blank">c++, <br> c</a> <br></div><div class="fright">}</div> <br></div><div id="frameworks" class="listcon"> <span class="blockt">Frameworks:</span> <br><div class="fleft">{</div><div class="inblock"> <br> <a href="http://nodejs.org/" class="ref" target="_blank">node.js</a>, <br> <a href="http://jquery.com/" class="ref" target="_blank">jQuery</a>, <br> <a href="http://www.apachefriends.org/en/xampp-linux.html" class="ref" target="_blank">LAMPP</a> <br></div><div class="fright">}</div> <br></div><div id="databasesoftware" class="listcon"> <span class="blockt">Database software:</span> <br><div class="fleft">{</div><div class="inblock"> <br> <a href="http://www.mongodb.org/" class="ref" target="_blank">mongodb</a>, <br> <a href="http://www.mysql.com/" class="ref" target="_blank">mysql</a> <br></div><div class="fright">}</div> <br></div><div id="syntax" class="listcon"> <span class="blockt">Syntax:</span> <br><div class="fleft">{</div><div class="inblock"> <br> <a href="http://github.github.com/github-flavored-markdown/" class="ref" target="_blank">markdown</a>, <br><a href="http://www.gnu.org/software/make/manual/make.html#toc_Introduction" class="ref" target="_blank">makefile</a>, <br> <a href="http://en.wikipedia.org/wiki/Diff" class="ref" target="_blank">diff</a>, <br> <a href="http://www.w3.org/html/" class="ref" target="_blank">html</a>, <br> <a href="http://www.json.org/" class="ref" target="_blank">json</a>, <br> <a href="http://www.w3.org/XML/" class="ref" target="_blank">xml</a>, <br> <a href="http://www.w3.org/Style/CSS/" class="ref" target="_blank">css</a> <br></div><div class="fright">}</div> <br></div><div id="shellscripting" class="listcon"> <span class="blockt">Shell scripting:</span> <br><div class="fleft">{</div><div class="inblock"> <br> <a href="http://en.wikipedia.org/wiki/Batch_file" class="ref" target="_blank">Windows batch</a>, <br> <a href="http://en.wikipedia.org/wiki/Bash_(Unix_shell)" class="ref" target="_blank">Linux bash</a> <br></div><div class="fright">}</div> <br></div><div id="footer">(c) 2012 David Volm <br><a href="https://github.com/daxxog/daxxog.com" target="_blank" class="footer">[view source on github]</a></div></body></html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>daXXog.com â€” homepage of David Volm
+    </title>
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono' rel='stylesheet' type='text/css'>
+    <link type="text/css" rel="stylesheet" href="./daxxog.css">
+    <!--[if !IE]>-->
+    <link type="text/css" rel="stylesheet" href="./notie.css">
+    <link type="text/css" rel="stylesheet" media="only screen and (max-device-width: 480px)" href="./iphone.css">
+    <!--<![endif]-->
+    <link rel="icon" type="image/vnd.microsoft.icon" href="./favicon.ico">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+<script type="text/javascript" src="./jquery-1.8.2.min.js"></script>
+<script type="text/javascript">
+$(function() {
+    $('div').animate({
+        opacity: 0.95 //95%
+    }, 1000);
+    $('body').scrollTop(1);
+});
+</script>
+  </head>
+  <body>
+    <div id="name">David Volm
+    </div>
+    <div class="hashtag"> 
+      <a class="hashtag" href="https://twitter.com/daxxog" target="_blank">@daxxog</a>
+    </div>
+    <div id="bio">I am a problem solver; 
+      <a class="bio" href="https://github.com/daxxog" target="_blank">in code</a> and everyday life. Self motivation and teamwork are critical skills. 
+      <a  class="bio" href="http://lcms.org/" target="_blank">Jesus Christ died to save me.</a>
+    </div>
+    <div id="programminglanguages" class="listcon"> 
+      <span class="blockt">Programming languages:
+      </span> 
+      <br>
+      <div class="fleft">{
+      </div>
+      <div class="inblock">
+        <br> 
+        <a href="https://developer.mozilla.org/en-US/docs/JavaScript" class="ref" target="_blank">javascript</a>, 
+        <br> 
+        <a href="http://www.oracle.com/technetwork/java/index.html" class="ref" target="_blank">java</a>, 
+        <br> 
+        <a href="http://php.net/" class="ref" target="_blank">php</a>, 
+        <br> 
+        <a href="http://gcc.gnu.org/" class="ref" target="_blank">c++, 
+          <br> c</a> 
+        <br>
+      </div>
+      <div class="fright">}
+      </div> 
+      <br>
+    </div>
+    <div id="frameworks" class="listcon">
+      <span class="blockt">Frameworks:
+      </span> 
+      <br>
+      <div class="fleft">{
+      </div>
+      <div class="inblock">
+        <br> 
+        <a href="http://nodejs.org/" class="ref" target="_blank">node.js</a>, 
+        <br> 
+        <a href="http://jquery.com/" class="ref" target="_blank">jQuery</a>, 
+        <br> 
+        <a href="http://www.apachefriends.org/en/xampp-linux.html" class="ref" target="_blank">LAMPP</a> 
+        <br>
+      </div>
+      <div class="fright">}
+      </div> 
+      <br>
+    </div>
+    <div id="databasesoftware" class="listcon"> 
+      <span class="blockt">Database software:
+      </span> 
+      <br>
+      <div class="fleft">{
+      </div>
+      <div class="inblock">
+        <br> 
+        <a href="http://www.mongodb.org/" class="ref" target="_blank">mongodb</a>, 
+        <br> 
+        <a href="http://www.mysql.com/" class="ref" target="_blank">mysql</a> 
+        <br>
+      </div>
+      <div class="fright">}
+      </div> 
+      <br>
+    </div>
+    <div id="syntax" class="listcon"> 
+      <span class="blockt">Syntax:
+      </span> 
+      <br>
+      <div class="fleft">{
+      </div>
+      <div class="inblock">
+        <br> 
+        <a href="http://github.github.com/github-flavored-markdown/" class="ref" target="_blank">markdown</a>, 
+        <br> 
+        <a href="http://www.gnu.org/software/make/manual/make.html#toc_Introduction" class="ref" target="_blank">makefile</a>, 
+        <br> 
+        <a href="http://en.wikipedia.org/wiki/Diff" class="ref" target="_blank">diff</a>, 
+        <br> 
+        <a href="http://www.w3.org/html/" class="ref" target="_blank">html</a>, 
+        <br> 
+        <a href="http://www.json.org/" class="ref" target="_blank">json</a>, 
+        <br> 
+        <a href="http://www.w3.org/XML/" class="ref" target="_blank">xml</a>, 
+        <br> 
+        <a href="http://www.w3.org/Style/CSS/" class="ref" target="_blank">css</a> 
+        <br>
+      </div>
+      <div class="fright">}
+      </div> 
+      <br>
+    </div>
+    <div id="shellscripting" class="listcon"> 
+      <span class="blockt">Shell scripting:
+      </span> 
+      <br>
+      <div class="fleft">{
+      </div>
+      <div class="inblock">
+        <br> 
+        <a href="http://en.wikipedia.org/wiki/Batch_file" class="ref" target="_blank">Windows batch</a>, 
+        <br>
+        <a href="http://en.wikipedia.org/wiki/Bash_(Unix_shell)" class="ref" target="_blank">Linux bash</a> 
+        <br>
+      </div>
+      <div class="fright">}
+      </div> 
+      <br>
+    </div>
+    <div id="footer">(c) 2012 David Volm 
+      <br>
+      <a href="https://github.com/daxxog/daxxog.com" target="_blank" class="footer">[view source on github]</a>
+    </div>
+  </body>
+</html>
